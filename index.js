922d8d3e5a710b7e62d5b83570ba255d34608785
Gilles Coomans <gilles.coomans@gmail.com>
2016-05-10 22:44:59 +0200

    debug history bind

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ y.Template.prototype.clickTo = function(href, title, data) {
 	return this.dom(function(context, node) {
 		node.addEventListener('click', function(e) {
 			e.preventDefault();
-			context.navigateTo(href, title, data);
+			context.navigateTo(href || node.getAttribute('href'), title, data);
 		});
 	});
 };
@@ -20,6 +20,7 @@ y.Template.prototype.aNav = function(href, title, content) {
 };
 
 y.Context.prototype.navigateTo = function(href, title, data) {
+	// console.log('navigateTo', href, title, data)
 	if (href !== router.current) {
 		router.current = href;
 		// update hash
@@ -51,36 +52,35 @@ var router = y.router = {
 		this.current = href;
 	},
 	bindHistory: function(context) {
-		if (!context.env.data.isServer) {
-			this.current = location.pathname + (location.search || '');
-			var route = utils.parseURL(this.current);
-			console.log('router bind to history', route, this.current)
-			context.isRouted = true;
-			var self = this;
+		if (context.env.data.isServer)
+			return;
+		this.current = location.pathname + (location.search || '');
+		var route = utils.parseURL(this.current);
+		console.log('yamvish router : bind to history', route, this.current)
+		context.isRouted = true;
+		var self = this;
+		context.set('$route', route);
+		router.oldRoute = route;
+		context.onAgora('route:update', function(context, route) {
+			router.oldRoute = context.data.$route;
 			context.set('$route', route);
-			router.oldRoute = route;
-			context.onAgora('route:update', function(context, route) {
-				router.oldRoute = context.data.$route;
-				context.set('$route', route);
-			});
-			// popstate event from back/forward in browser
-
-			window.addEventListener('popstate', function(e) {
-				var newURL = location.pathname + (location.search || '');
-				// console.log('pop state : ', e, newURL);
-				if (newURL === self.current) {
-					// console.log('same url : so return');
-					return;
-				}
-				self.current = newURL;
-				var route = utils.parseURL(newURL);
-				context.toAgora('route:update', route);
-				document.title = e.state ? (e.state.title || '') : '';
-			});
-			// window.addEventListener('hashchange', function(e) {
-			// 	console.log('hashchange : ', e);
-			// });
-		}
+		});
+		// popstate event from back/forward in browser
+		window.addEventListener('popstate', function(e) {
+			var newURL = location.pathname + (location.search || '');
+			// console.log('pop state : ', e, newURL);
+			if (newURL === self.current) {
+				console.log('router popstate : same url : so return');
+				return;
+			}
+			self.current = newURL;
+			var route = utils.parseURL(newURL);
+			context.toAgora('route:update', route);
+			document.title = e.state ? (e.state.title || '') : '';
+		});
+		// window.addEventListener('hashchange', function(e) {
+		// 	console.log('hashchange : ', e);
+		// });
 	}
 };
 
